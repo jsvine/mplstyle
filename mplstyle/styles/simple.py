18c707bbf877391ec34bfa060b1f9a71681fe650
Jeremy Singer-Vine <jsvine@gmail.com>
2014-06-10 10:59:44 -0400

    Fix tabs

--- mplstyle/styles/simple.py
+++ mplstyle/styles/simple.py
@@ -6,44 +6,44 @@ DARK_GRAY = "#555555"
 LIGHT_GRAY = "#EFEFEF"
 
 def style(palette=mpl.cm.Set1, n_colors=MAX_COLORS):
-	colors = map(mpl.colors.rgb2hex, palette([
-	    float(x)/(MAX_COLORS) for x in range(MAX_COLORS)
-	]))[:min(MAX_COLORS, n_colors)]
-	return {
-		"axes": {
-			"color_cycle": colors,
-			"facecolor": "white",
-			"edgecolor": DARK_GRAY,
-			"grid": True,
-			"titlesize": "x-large",
-			"labelsize": "x-large",
-			"labelcolor": "#555555",
-			"axisbelow": True
-		},
-		"patch": {
-			"facecolor": colors[0]	
-		},
-		"lines": {
-			"color": "r",
-			"linewidth": 2,
-			"marker": "o",
+    colors = map(mpl.colors.rgb2hex, palette([
+        float(x)/(MAX_COLORS) for x in range(MAX_COLORS)
+    ]))[:min(MAX_COLORS, n_colors)]
+    return {
+        "axes": {
+            "color_cycle": colors,
+            "facecolor": "white",
+            "edgecolor": DARK_GRAY,
+            "grid": True,
+            "titlesize": "x-large",
+            "labelsize": "x-large",
+            "labelcolor": "#555555",
+            "axisbelow": True
+        },
+        "patch": {
+            "facecolor": colors[0]	
+        },
+        "lines": {
+            "color": "r",
+            "linewidth": 2,
+            "marker": "o",
             "markersize": 4,
             "markeredgewidth": 0
-		},
-		"figure": {
-			"facecolor": "white",
-			"edgecolor": DARK_GRAY
-		},
-		"ytick": {
-			"color": DARK_GRAY,
-			"direction": "out"
-		},
-		"xtick": {
-			"color": DARK_GRAY,
-			"direction": "out"
-		},
-		"grid": {
-			"color": DARK_GRAY,
-			"linestyle": ':'
-		}
-	}
+        },
+        "figure": {
+            "facecolor": "white",
+            "edgecolor": DARK_GRAY
+        },
+        "ytick": {
+            "color": DARK_GRAY,
+            "direction": "out"
+        },
+        "xtick": {
+            "color": DARK_GRAY,
+            "direction": "out"
+        },
+        "grid": {
+            "color": DARK_GRAY,
+            "linestyle": ':'
+        }
+    }
